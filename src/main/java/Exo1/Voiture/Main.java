7515e4f0571f46381a5a41988de14d570241301a
ythai <yvanthai@gmail.com>
2023-01-03 12:01:01 +0100

    exo116

--- src/main/java/Exo1/Voiture/Main.java
+++ src/main/java/Exo1/Voiture/Main.java
@@ -3,13 +3,7 @@ package Exo1.Voiture;
 public class Main {
 
     public static void main (String[] args){
-        Voiture voiture1 = new Voiture("blanc", "Renault", "Megane");
-        voiture1.demarrer();
-        voiture1.avancer(50);
-        voiture1.avancer(-40);
-        voiture1.setCouleur("rose");
-        voiture1.setMarque("Honda");
+        Voiture voiture1 = new Voiture();
         voiture1.afficher();
-
     }
 }
--- src/main/java/Exo1/Voiture/Voiture.java
+++ src/main/java/Exo1/Voiture/Voiture.java
@@ -13,7 +13,11 @@ public class Voiture {
         setModele(modele);
     }
 
-    public Voiture(){}
+    public Voiture(){
+        this.couleur = "blanc";
+        this.marque = "Renault";
+        this.modele = "Megane";
+    }
 
     public void demarrer(){
         demarre = true;
