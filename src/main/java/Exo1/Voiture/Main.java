c7145a2d15f24d9fcc8ff5c8258bc02c0b286dfc
ythai <yvanthai@gmail.com>
2023-01-03 11:58:57 +0100

    exo113

--- src/main/java/Exo1/Voiture/Main.java
+++ src/main/java/Exo1/Voiture/Main.java
@@ -3,15 +3,14 @@ package Exo1.Voiture;
 public class Main {
 
     public static void main (String[] args){
-        Voiture voiture1 = new Voiture(null,"Renault", "Megane");
-        Voiture voiture2 = new Voiture(null,"Peugeot", "208");
-        Voiture voiture3 = new Voiture(null,"Citroen", "C3");
+        Voiture voiture1 = new Voiture("rouge","Renault", "Megane");
+        Voiture voiture2 = new Voiture("bleue","Peugeot", "208");
+        Voiture voiture3 = new Voiture("verte","Citroen", "C3");
         voiture1.demarrer();
         voiture2.demarrer();
         voiture3.demarrer();
-        voiture1.avancer(50);
+        voiture1.avancer(70);
         voiture2.avancer(20);
-        voiture1.avancer(20);
         voiture1.afficher();
         voiture2.afficher();
         voiture3.afficher();
