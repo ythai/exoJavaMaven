9ac363cc973179504fa04bfa24135e368c56d8b0
ythai <yvanthai@gmail.com>
2023-01-03 12:07:28 +0100

    exo121 et 122

---
+++ src/main/java/Exo1/Voiture/Camion.java
@@ -0,0 +1,29 @@
+package Exo1.Voiture;
+
+public class Camion extends Vehicule {
+
+    private int charge;
+
+    public int getCharge() {
+        return charge;
+    }
+
+    public void setCharge(int charge) {
+        this.charge=charge;
+
+    }
+
+    public void modifierCharge(int chargeAModifier){
+        int chargeFinal = charge + chargeAModifier;
+        if(chargeFinal < 0)
+            System.out.println("Charge negatif interdite:" + chargeFinal);
+        else
+            setCharge(chargeFinal);
+    }
+
+    @Override
+    public void afficher() {
+        super.afficher();
+        System.out.println("Charge : " + charge);
+    }
+}
--- src/main/java/Exo1/Voiture/Main.java
+++ src/main/java/Exo1/Voiture/Main.java
@@ -1,9 +1,13 @@
 package Exo1.Voiture;
 
-public class Main {
+public class Main{
 
     public static void main (String[] args){
-        Voiture voiture1 = new Voiture();
-        voiture1.afficher();
+        Camion camion1 = new Camion();
+        camion1.setModele("T440");
+        camion1.setCharge(300);
+        camion1.modifierCharge(-50);
+        camion1.modifierCharge(-400);
+        camion1.afficher();
     }
 }
